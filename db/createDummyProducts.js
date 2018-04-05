77727b94a3ba1d0eaf6039c0df77e99873cd8433
Muqsith Irfan <mui@one.com>
2018-04-05 14:58:08 +0400

    First commit

---
+++ db/createDummyProducts.js
@@ -0,0 +1,16 @@
+let products = [
+    {
+        'title': 'p1',
+        'description': 'kldfkdfdkf ....',
+        'price': 24,
+        'images': ['dlkfdkjfdf'],
+        'stock': 20
+    },
+    {
+        'title': 'p1',
+        'description': 'kldfkdfdkf ....',
+        'price': 24,
+        'images': ['dlkfdkjfdf'],
+        'stock': 20
+    }
+]
---
+++ db/models/product.js
@@ -0,0 +1,9 @@
+const PRODUCT = {
+    'title': String,
+    'description': String,
+    'price': Number,
+    'images': Array,
+    'stock': Number
+};
+
+module.exports = PRODUCT;
